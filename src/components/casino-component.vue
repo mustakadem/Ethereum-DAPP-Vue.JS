bc9014a58d36506c7f22418f05a9b0b0fc2764bb
musta <mustakadem@gmail.com>
2018-06-08 13:06:28 +0200

    probando Ethereum con web3 y vue

---
+++ src/components/casino-component.vue
@@ -0,0 +1,108 @@
+<template>
+<div class="casino">
+  <h1>Bienvenido a la prueba de red Ethereum</h1>
+  <h4>Selecciona un numero entre 1 y 10</h4>
+  Cantidad apostada: <input v-model="amount" placeholder="0 Ether">
+  <ul>
+    <li @click="clickNumber">1</li>
+    <li @click="clickNumber">2</li>
+    <li @click="clickNumber">3</li>
+    <li @click="clickNumber">4</li>
+    <li @click="clickNumber">5</li>
+    <li @click="clickNumber">6</li>
+    <li @click="clickNumber">7</li>
+    <li @click="clickNumber">8</li>
+    <li @click="clickNumber">9</li>
+    <li @click="clickNumber">10</li>
+  </ul>
+  <img v-if='pending' id='loader' src='https://loading.io/spinners/double-ring/lg.double-ring-spinner.gif'>
+  <div class="event" v-if="winEvent">
+    Won: {{winEvent._status}}
+    Amount: {{winEvent._amount}} Wei
+  </div>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'casino',
+    data () {
+      return {
+        amount: null,
+        pending: false,
+        winEvent: null
+      }
+    },
+    methods: {
+      clickNumber (event) {
+        console.log('BETTING ON NUMBER, AMOUNT', event.target.innerHTML, this.amount);
+        this.winEvent = null;
+        this.pending = true;
+        this.$store.state.contractInstance().bet(event.target.innerHTML, {
+          gas: 300000,
+          value: this.$store.state.web3.web3Instance().toWei(this.amount, 'ether'),
+          from: this.$store.state.web3.coinbase
+        }, (err, result) => {
+          if (err) {
+            console.log(err);
+            this.pending = false
+          } else {
+            let Won = this.$store.state.contractInstance().Won();
+            Won.watch((err, result) => {
+              if (err) {
+                console.log('could not get event Won()')
+              } else {
+                this.winEvent = result.args;
+                this.winEvent._amount = parseInt(result.args._amount, 10);
+                this.pending = false
+              }
+            })
+          }
+        })
+      }
+    },
+    mounted () {
+      console.log('dispatching getContractInstance');
+      this.$store.dispatch('getContractInstance')
+    }
+  }
+</script>
+
+<style scoped>
+  .casino {
+    margin-top: 50px;
+    text-align:center;
+  }
+  #loader {
+    width:150px;
+  }
+  ul {
+    margin: 25px;
+    list-style-type: none;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-column-gap:25px;
+    grid-row-gap:25px;
+  }
+  li{
+    padding: 20px;
+    margin-right: 5px;
+    border-radius: 50%;
+    cursor: pointer;
+    background-color:#fff;
+    border: -2px solid #bf0d9b;
+    color: #bf0d9b;
+    box-shadow:3px 5px #bf0d9b;
+  }
+  li:hover{
+    background-color:#bf0d9b;
+    color:white;
+    box-shadow:0px 0px #bf0d9b;
+  }
+  li:active{
+    opacity: 0.7;
+  }
+  *{
+    color: #444444;
+  }
+</style>
---
+++ src/components/casino-dapp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <casino></casino>
+    <hello-metamask></hello-metamask>
+  </div>
+</template>
+
+<script>
+  import HelloMetamask from './hello-metamask';
+  import Casino from './casino-component';
+
+  export default {
+        name: "casino-dapp",
+      beforeCreate(){
+        console.log('accion de registerWeb3 en casino-dapp');
+        this.$store.dispatch('registerWeb3');
+      },
+      components:{
+          'hello-metamask': HelloMetamask,
+          'casino': Casino
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
